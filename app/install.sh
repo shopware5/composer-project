61e95c0970643998b2226e5dd51426149dec45b6
Hendrik Söbbing <h.soebbing@shopware.com>
2018-01-17 10:12:04 +0100

    Improve support for case sensitivity in queries

--- app/install.sh
+++ app/install.sh
@@ -13,14 +13,16 @@ loadEnvFile
 
 BC=$'\e[41m\e[97m'
 EC=$'\e[0m'
-read -p "Start installation? This will drop the database ${BC}$DATABASE_URL${EC}! (y/N) " DROP_DATABASE
 
-DROP_DATABASE=${DROP_DATABASE:-"n"}
-[ $DROP_DATABASE = n ] && exit 1;
+DROP_DATABASE=$(promptYesOrNo "Start installation? This will drop the database ${BC}$DATABASE_URL${EC}! (y/N) " 'n')
+if [ $DROP_DATABASE = n ]; then
+    echo -e "\nNot touching the database, have fun!\n"
+    exit 0;
+fi
 
 swCommand sw:database:setup --steps=drop,create,import
 
-if [ $IMPORT_DEMODATA = y ] ; then
+if [ $IMPORT_DEMODATA = y ]; then
    echo "Importing demo data please wait..."
    swCommand sw:database:setup --steps=importDemodata
 fi
@@ -33,11 +35,12 @@ swCommand sw:theme:initialize
 swCommand sw:firstrunwizard:disable
 swCommand sw:admin:create --name="$ADMIN_NAME" --email="$ADMIN_EMAIL" --username="$ADMIN_USERNAME" --password="$ADMIN_PASSWORD" -n
 
-if [ $IMPORT_DEMODATA = y ] ; then
-    read -p "Do you want to install the images (~285MB) for the installed demo data? cURL is required. (Y/n) " INSTALL_IMAGES
-    INSTALL_IMAGES=${INSTALL_IMAGES:-"y"}
+if [ $IMPORT_DEMODATA = y ]; then
+    INSTALL_IMAGES=$(promptYesOrNo "Do you want to install the images (~285MB) for the installed demo data? cURL is required. (Y/n) " 'y')
 
-    [ $INSTALL_IMAGES = y ] && `which curl` -L "http://releases.s3.shopware.com/test_images_since_5.1.zip" > images.zip && unzip images.zip && rm images.zip
+    if [ $INSTALL_IMAGES = y ]; then
+        `which curl` -L "http://releases.s3.shopware.com/test_images_since_5.1.zip" > images.zip && unzip images.zip && rm images.zip
+    fi
 fi
 
 echo -e "\nInstallation finished, have fun!\n"
