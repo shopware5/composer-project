4e2df10db41593d261bfe3207345eebf4bb11b0d
Hendrik Söbbing <hendrik@soebbing.de>
2021-10-05 14:44:20 +0200

    Change default value to `null`

--- app/config/config.php
+++ app/config/config.php
@@ -19,7 +19,7 @@ $composerConfig = [
         'password' => $db['pass'],
         'dbname'   => $db['path'],
         'host'     => $db['host'],
-        'port'     => $db['port'] ?? 3306,
+        'port'     => $db['port'] ?? null,
     ],
 
     'template' => [
